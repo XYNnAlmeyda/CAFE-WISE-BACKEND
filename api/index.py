1ad9e79fb085c04059ff24354f1f6af6c6c81d96
Xynn Almeyda <almeydazhen@gmail.com>
2026-09-02 00:58:41 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -57,6 +57,8 @@ app.include_router(activity_router)
 app.include_router(shifts_router)
 
 @app.get("/")
+@app.get("/api")
+@app.get("/api/index.py")
 def read_root():
     return {"message": "Welcome to the CafeWise Python Backend"}
 
